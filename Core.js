3462d48bf47b42057764b830223697eea3bc0ab3
Enzo HOURLAY <enzo.hourlay@etu.univ-amu.fr>
2024-04-02 20:35:29 +0200

    Add quiz questions to App.js

--- Core.js
+++ Core.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Core() {
   return (
     <View style={styles.container}>
         <Text>Open up App.js to start working on your App!</Text>
